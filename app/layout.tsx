afc9f3a27e85cacf71e114c31f424264adc380f2
Sumit Pandey <sumit.pandeyy05@gmail.com>
2026-05-19 19:01:48 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import { DM_Sans, Newsreader } from "next/font/google";
 import "./globals.css";
 
@@ -53,7 +54,10 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
